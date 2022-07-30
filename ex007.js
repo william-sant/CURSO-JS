3fdcbb17ed03e4d7312310e1285c29379debc850
William Santana <81190663+william-sant@users.noreply.github.com>
2022-07-30 17:36:56 -0300

    Initial commit

---
+++ ex007.js
@@ -0,0 +1,6 @@
+var vel = 40
+console.log(`a velocidade do do seu carro é ${vel} km/hr`)
+if (vel > 60) { //condição simples
+    console.log(`Você ultrapassou a velocidade eprmitida. MULDADO!`)
+}
+console.log(`dirija sempre usando cinto de segurança!`)
